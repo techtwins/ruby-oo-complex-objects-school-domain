8e1aa788cba7169f777476efd418c8f3f6a9ab74
techtwins <gshevaughn@gmail.com>
2020-09-15 11:46:32 -0400

    all conditions passing

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+
+    def initialize (name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student (name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << name
+    end
+
+    def grade (student_grade)
+        @roster[student_grade]
+    end
+
+    def sort
+        roster.each {
+            |key, val| 
+            roster[key] = val.sort
+        }
+    end
+end 
